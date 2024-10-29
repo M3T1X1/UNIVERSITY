6ea828f734115c70da1661f0e8753c3b9c00a209
M3T1X1 <dusza-kacper@wp.pl>
2024-10-29 23:18:04 +0100

    same as before

--- semester_3/Python/Lab2/test.py
+++ semester_3/Python/Lab2/test.py
@@ -1,15 +1,11 @@
+elements = [1, "2", None, 0, False, True, 4, "3", 4, 3, False]
+count_dict = {}
 
-list_a = [1, 2, 3]
-list_b = [2, 3, 4]
+for elem in elements:
+    key = (elem, type(elem))
+    if key in count_dict:
+        count_dict[key] += 1
+    else:
+        count_dict[key] = 1
 
-listaNowa =[]
-
-
-
-for i in range(list_a.__len__()):
-    for j in range(list_b.__len__()):
-        if list_a[i] == list_b[j]:
-            listaNowa.append(list_a[i])
-
-for i in range(listaNowa.__len__()):
-    print(listaNowa[i])
+print(count_dict)
