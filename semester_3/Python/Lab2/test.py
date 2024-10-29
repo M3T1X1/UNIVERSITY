be50832cca9786dbf28675446af5c7c85b77d293
M3T1X1 <dusza-kacper@wp.pl>
2024-10-29 22:46:43 +0100

    did some python exercises

--- semester_3/Python/Lab2/test.py
+++ semester_3/Python/Lab2/test.py
@@ -1,6 +1,15 @@
-def shift_list(input_list: list, n: int) -> list:
-    return ...
 
-input_list = [1, 2, 3, 4, 5]
-input_list_shifted = shift_list(input_list, 3)
-print(input_list_shifted)
+list_a = [1, 2, 3]
+list_b = [2, 3, 4]
+
+listaNowa =[]
+
+
+
+for i in range(list_a.__len__()):
+    for j in range(list_b.__len__()):
+        if list_a[i] == list_b[j]:
+            listaNowa.append(list_a[i])
+
+for i in range(listaNowa.__len__()):
+    print(listaNowa[i])
