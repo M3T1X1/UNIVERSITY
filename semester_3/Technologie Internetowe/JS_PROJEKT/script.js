088cd0927fdcdfa6396b21ea06cf311f3b1312f0
M3T1X1 <dusza-kacper@wp.pl>
2025-01-08 21:02:05 +0100

    JS project progression

--- semester_3/Technologie Internetowe/JS_PROJEKT/script.js
+++ semester_3/Technologie Internetowe/JS_PROJEKT/script.js
@@ -1,4 +1,34 @@
-function resetDropdowns() {
+function resetDropdowns() 
+{
     const dropdowns = document.querySelectorAll('select');
     dropdowns.forEach(dropdown => dropdown.selectedIndex = 0);
 }
+
+function valuePrint()
+{
+    const zespol = document.getElementById('zespół');
+    const zespolValue = zespol.options[zespol.selectedIndex].text;
+
+    const lokalizacja = document.getElementById('lokalizacja');
+    const lokalizacjaValue = lokalizacja.options[lokalizacja.selectedIndex].text;
+
+    const sektor = document.getElementById('sektor');
+    const sektorValue = sektor.options[sektor.selectedIndex].text;
+
+
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `Twoje wybory: ${zespolValue},  ${lokalizacjaValue}, ${sektorValue}`;
+}
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const countBtn = document.getElementById('count');
+    const resetBtn = document.getElementById('reset');
+
+    countBtn.addEventListener('click', () =>{
+        valuePrint();
+        })
+    resetBtn.addEventListener('click', () =>{
+        resetDropdowns();
+        })
+    })
+
