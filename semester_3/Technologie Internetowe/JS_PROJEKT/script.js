327478cb5172f57c8ffa2814167470ee1b2e33ea
M3T1X1 <dusza-kacper@wp.pl>
2025-01-10 00:05:43 +0100

    added a new function

--- semester_3/Technologie Internetowe/JS_PROJEKT/script.js
+++ semester_3/Technologie Internetowe/JS_PROJEKT/script.js
@@ -1,3 +1,7 @@
+function onloadAlert()
+{
+    alert("Kod zniżkowy 10%: Kod");
+}
 function resetDropdowns() 
 {
     const dropdowns = document.querySelectorAll('select');
@@ -15,10 +19,92 @@ function valuePrint()
     const sektor = document.getElementById('sektor');
     const sektorValue = sektor.options[sektor.selectedIndex].text;
 
-
-    const resultElement = document.getElementById('result');
-    resultElement.textContent = `Twoje wybory: ${zespolValue},  ${lokalizacjaValue}, ${sektorValue}`;
+    const resultElement = document.getElementById('resultChoices');
+    if(zespolValue == "Wybierz zespół" || lokalizacjaValue == "Wybierz lokalizacje" || sektorValue =="Wybierz sektor")
+    {
+        return -1;
+    }
+    else
+    {
+    resultElement.textContent = `${zespolValue},  ${lokalizacjaValue}, ${sektorValue}`;
+    }
 }
+
+function priceCounter() 
+{
+    let basePrice = 100;
+    let currentValue = 0;
+
+    const zespol = document.getElementById('zespół');
+    const zespolValue = zespol.options[zespol.selectedIndex]?.text;
+
+    if(zespolValue === "Amon Amarth") 
+    {
+        currentValue = basePrice * 1.2;
+    } 
+    else if(zespolValue === "Vader") 
+    {
+    currentValue = basePrice * 1.4;
+    } 
+    else if(zespolValue === "Arch Enemy") 
+    {
+        currentValue = basePrice * 1.6;
+    } 
+    else 
+    {
+        alert("Wybierz zespół!");
+        return -1;
+    }
+
+    const lokalizacja = document.getElementById('lokalizacja');
+    const lokalizacjaValue = lokalizacja.options[lokalizacja.selectedIndex]?.text;
+
+    if(lokalizacjaValue === "Warszawa") 
+    {
+        currentValue *= 2;
+    } 
+    else if(lokalizacjaValue === "Kraków") 
+    {
+        currentValue *= 1.8;
+    } 
+    else if(lokalizacjaValue === "Rzeszów") 
+    {
+        currentValue *= 1.4;
+    } 
+    else 
+    {
+        alert("Wybierz lokalizację!");
+        return -1;
+    }
+
+    const sektor = document.getElementById('sektor');
+    const sektorValue = sektor.options[sektor.selectedIndex]?.text;
+
+    if (sektorValue === "Miejsca pod sceną") {
+        currentValue *= 3;
+    } else if (sektorValue === "Płyta") {
+        currentValue *= 1.4;
+    } else if (sektorValue === "Trybuny") {
+        currentValue *= 1.2;
+    } else {
+        alert("Wybierz sektor!");
+        return -1;
+    }
+
+    const kodPromocyjny = document.getElementById('kodPromocyjny').value.trim();
+
+    if(kodPromocyjny == "Kod") 
+    {
+        currentValue *= 0.9; 
+    }
+    
+
+    const resultValue = document.getElementById('resultValue');
+    resultValue.textContent = `Cena: ${currentValue.toFixed(2)} zł`;
+}
+
+
+
 
 document.addEventListener("DOMContentLoaded", () =>{
     const countBtn = document.getElementById('count');
@@ -26,6 +112,7 @@ document.addEventListener("DOMContentLoaded", () =>{
 
     countBtn.addEventListener('click', () =>{
         valuePrint();
+        priceCounter();
         })
     resetBtn.addEventListener('click', () =>{
         resetDropdowns();
